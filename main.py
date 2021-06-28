031c6dc510bf1e45fc9f5bb47ca9ad63fb228eb9
Cosinusjustafonction <64425437+Cosinusjustafonction@users.noreply.github.com>
2021-06-28 18:59:35 -0500

    gui imporvement

--- main.py
+++ main.py
@@ -1,29 +1,51 @@
 import pyglet
-from minigolf.Lib.vectors import vec3d
-from minigolf.Lib.physics import Displacement
+import numpy as np 
+import math 
 window = pyglet.window.Window(resizable=True)
 
-class Ball:
-	def __init__(self,position,radius):
-		self.shape = self.ball = pyglet.shapes.Circle( x=self.ball_position[0], y=self.ball_position[1],
-		                                  radius=0.01 * max( window.height, window.width ), color=(255, 255, 255))
-		self.displacement = Displacement(position,vec3d(0,0,0),vec3d(0,0,-10))
 class GolfCourse:
 
 	def __init__(self,hole_position,ball_position):
-		self.hole_position = vec2d(hole_position)
-		self.ball = Ball()
+		self.hole_position = hole_position
+		self.ball_position = ball_position
 		self.batch = pyglet.shapes.Batch()
 		self.course = pyglet.shapes.Rectangle(width=window.width, height=window.height,x=0,y=0,color=(13, 143, 26),batch=self.batch)
 		self.hole = pyglet.shapes.Circle(x=self.hole_position[0],y=self.hole_position[1],radius=0.01*max(window.height,window.width),color=(0,0,0),batch=self.batch)
-
+		self.ball = pyglet.shapes.Circle( x=self.ball_position[0], y=self.ball_position[1],
+		                                  radius=0.01 * max( window.height, window.width ), color=(255, 255, 255),
+		                                  batch=self.batch )
+		self.radius = 0.01*max(window.height,window.width)
+		self.isdraw = False 
+	def draw_rect(self,dx,dy) : 
+		self.x = self.ball_position[0]
+		self.y = self.ball_position[1]
+		self.dx = dx
+		self.dy = dy
+		self.toul = (150+200)-(dx+dy)
+		if self.toul > 100 :
+			self.toul = 100
+		elif self.toul < -100 : 
+			self.toul = -100
+		self.arrow= pyglet.shapes.Rectangle(width=self.toul, height=5,x=self.x,y=self.y,color=(0, 25, 77),batch=self.batch,)
+		self.arrow.rotation = (180 / math.pi) * math.atan2(self.dy, self.dx)#gets angle of the arrow 
+		self.batch.draw()
 	def draw(self):
 		self.batch.draw()
 
 golf_course = GolfCourse((10,10),(200,150))
-pyglet.clock.schedule_interval(lambda x: print(x),1/60)
+
 @window.event()
 def on_draw():
 	window.clear()
 	golf_course.draw()
+	
+@window.event() 
+def on_mouse_drag(x,y,dx,dy,button, modifiers) : 
+	main_pos = x, y
+	golf_course.draw_rect(main_pos[0],main_pos[1])
+	print(main_pos)
+@window.event()
+def on_mouse_release(x, y, button, modifiers):
+	golf_course.arrow.delete()
+	#here the mooving function
 pyglet.app.run()
