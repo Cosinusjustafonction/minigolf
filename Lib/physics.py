31537440db5426a4572b890dd3b8db121a98bacc
Cosinusjustafonction <64425437+Cosinusjustafonction@users.noreply.github.com>
2021-06-29 23:57:18 -0500

    smol pc update

--- Lib/physics.py
+++ Lib/physics.py
@@ -1,5 +1,5 @@
 from Lib.vectors import vec3d
-
+import math 
 class Displacement:
 	def __init__(self,position, speed,acceleration):
 		#setting some physical properties
@@ -30,6 +30,34 @@ class Displacement:
 		if abs(self.speed[1])<0.1:
 			self.speed[1]=0
 		self.speed*=self.coefficient_of_friction
+	def get_list(self,height , width):
+		big = []
+		if height > width : 
+		    for i in range(height) : 
+		        big.append((i, width))
+		else  :
+		    for i in range(width) : 
+		        big.append((i, height))
+		return big 
+	def get_list2(self,height , width) :	 
+		big = []
+		if height < width : 
+			for i in  range(height) : 
+				big.append((width,i ))
+		else : 
+			for i in range(width):
+				big.append((height, i))
+
+		return big 
+	def is_col(self,height,width,x,y,radius,pos) :
+		main_list = []
+		if pos == "left" : 
+			main_list = self.get_list(height,width)
+		else : 
+			main_list = self.get_list2(height,width)
+		for i in main_list : 
+			if ((i[0]-x)**2 +(i[1]-y)**2)<radius**2 : 
+				return True
 
 
 
@@ -43,6 +71,3 @@ def intersect_two_intervals(inter1,inter2):
 		return [sorted_values[1][0],sorted_values[0][1]]
 	else:
 		return None
-
-
-
